2be5efa9c8f87157ae8604f12b1622b85e2d89cf
Francesco Carrara <f.carrara24@studenti.unibg.it>
2024-04-10 16:35:40 +0200

    implemented simple test methods for testing

--- src/main/java/org/lessons/java/Calculator.java
+++ src/main/java/org/lessons/java/Calculator.java
@@ -3,22 +3,22 @@ package org.lessons.java;
 
 public class Calculator {
 
-    public float add(float num1, float num2) {
+    public static float add(float num1, float num2) {
         return num1 + num2;
     }
 
-    public float subtract(float num1, float num2) {
+    public static float subtract(float num1, float num2) {
         return num1 - num2;
     }
 
-    public float divide(float num1, float num2) {
+    public static float divide(float num1, float num2) {
         if (num2 == 0) {
             throw new IllegalArgumentException("Cannot divide by zero");
         }
         return num1 / num2;
     }
 
-    public float multiply(float num1, float num2) {
+    public static float multiply(float num1, float num2) {
         return num1 * num2;
     }
 
--- src/test/java/org/lessons/java/CalculatorTest.java
+++ src/test/java/org/lessons/java/CalculatorTest.java
@@ -8,18 +8,22 @@ class CalculatorTest {
 
     @Test
     void add() {
+        assertEquals(5.0f, Calculator.add(2.0f, 3.0f), 0.01);
     }
 
     @Test
     void subtract() {
+        assertEquals(-1.0f, Calculator.subtract(2.0f, 3.0f), 0.01);
     }
 
     @Test
     void divide() {
+        assertEquals(2.0f, Calculator.divide(6.0f, 3.0f), 0.001);
     }
 
     @Test
     void multiply() {
+        assertEquals(6.0f, Calculator.multiply(2.0f, 3.0f), 0.001);
     }
 
 }

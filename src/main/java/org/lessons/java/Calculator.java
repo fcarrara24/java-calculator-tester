a639fcf40dedbf83cfdc5da9aac62f4893387047
Francesco Carrara <f.carrara24@studenti.unibg.it>
2024-04-10 16:26:54 +0200

    setup pom file and test classes

--- src/main/java/org/lessons/java/Calculator.java
+++ src/main/java/org/lessons/java/Calculator.java
@@ -1,2 +1,26 @@
-package org.lessons.java;public class Calculator {
+package org.lessons.java;
+
+
+public class Calculator {
+
+    public float add(float num1, float num2) {
+        return num1 + num2;
+    }
+
+    public float subtract(float num1, float num2) {
+        return num1 - num2;
+    }
+
+    public float divide(float num1, float num2) {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return num1 / num2;
+    }
+
+    public float multiply(float num1, float num2) {
+        return num1 * num2;
+    }
+
+
 }
---
+++ src/test/java/org/lessons/java/CalculatorTest.java
@@ -0,0 +1,25 @@
+package org.lessons.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void divide() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+}
